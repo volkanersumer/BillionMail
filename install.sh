ff3b6837cd24266f82c65d8a34be104507bc73ae
aapanel.com <fan@aapanel.com>
2025-03-20 14:06:56 +0800

    Add SPF, DKIM, DMARC records
    1. Add SPF, DKIM, DMARC records
    
    2. Support using 'bash mail_users.sh show-record' to view domain DNS records

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ CONTAINER_PROJECT_NAME=billionmail
 PGSQL_CONTAINER_NAME="${CONTAINER_PROJECT_NAME}-pgsql-billionmail-1"
 DOVECOT_CONTAINER_NAME="${CONTAINER_PROJECT_NAME}-dovecot-billionmail-1"
 POSTFIX_CONTAINER_NAME="${CONTAINER_PROJECT_NAME}-postfix-billionmail-1"
+RSPAMD_CONTAINER_NAME="${CONTAINER_PROJECT_NAME}-rspamd-billionmail-1"
 create_time=$(date +%s)
 DBNAME=billionmail
 DBUSER=billionmail
@@ -106,7 +107,8 @@ echo -e ""
     echo -e "Mail Server hostname (FQDN), As: example.com"
     echo -e ""
     read -p "Please enter the Mail Server hostname (FQDN): " -e BILLIONMAIL_HOSTNAME
-    if [[ ! "${BILLIONMAIL_HOSTNAME}" =~ ^[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](\.[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9])*$ ]]; then
+    #if [[ ! "${BILLIONMAIL_HOSTNAME}" =~ ^[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](\.[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9])*$ ]]; then
+    if [[ ! "${BILLIONMAIL_HOSTNAME}" =~ ^([a-zA-Z0-9]([a-zA-Z0-9-]{0,62}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$ ]]; then
         echo -e "\e[31m(${BILLIONMAIL_HOSTNAME}) is not a FQDN!\e[0m"
         echo "Please change it to a FQDN"
         exit 1
@@ -1006,6 +1008,119 @@ Check_Connect_PgSql(){
 
 }
 
+
+Domain_DKIM_record(){
+    ## DKIM key generation
+    docker exec -i -e BILLIONMAIL_HOSTNAME=${BILLIONMAIL_HOSTNAME} ${RSPAMD_CONTAINER_NAME} bash -c 'cat << "EOF" > /tmp/1.sh
+#!/bin/bash
+if [ ! -d "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/" ]; then
+    mkdir -p "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/"
+fi
+if [ -f "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/default.private" ] && [ -f "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/default.pub" ]; then
+    #echo "DKIM key already exists, skipping generation."
+    exit 0
+fi
+
+rspamadm dkim_keygen -s 'default' -b 1024 -d {domain} -k "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/default.private" > "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/default.pub"
+if [ $? -eq 0 ]; then
+    # Define the root directory for private keys
+    DKIM_KEYS_DIR="/var/lib/rspamd/dkim"
+    # Configuration file path
+    CONFIG_FILE="/etc/rspamd/local.d/dkim_signing.conf"
+    # Check if the configuration file exists, if not, create it
+    if [ ! -f "${CONFIG_FILE}" ]; then
+        touch "${CONFIG_FILE}"
+        echo "${CONFIG_FILE}"
+        echo "domain {" > "${CONFIG_FILE}"
+        echo "#BT_DOMAIN_DKIM_BEGIN" >> "${CONFIG_FILE}"
+        echo "#BT_DOMAIN_DKIM_END" >> "${CONFIG_FILE}"
+        echo "}" >> "${CONFIG_FILE}"
+    fi
+    # Scan the DKIM_KEYS_DIR directory to get all domains
+    DOMAINS=()
+    for DOMAIN_DIR in "${DKIM_KEYS_DIR}"/*; do
+        if [ -d "${DOMAIN_DIR}" ]; then
+            DOMAIN_NAME=$(basename "${DOMAIN_DIR}")
+            PRIVATE_KEY_PATH="${DOMAIN_DIR}/default.private"
+            if [ -f "${PRIVATE_KEY_PATH}" ]; then
+                DOMAINS+=("${DOMAIN_NAME}:${PRIVATE_KEY_PATH}")
+            else
+                echo "Warning: Private key file ${PRIVATE_KEY_PATH} for domain ${DOMAIN_NAME} does not exist, skipping configuration."
+            fi
+        fi
+    done
+    # Add domain configurations
+    for DOMAIN in "${DOMAINS[@]}"; do
+        DOMAIN_NAME=$(echo "${DOMAIN}" | cut -d':' -f1)
+        PRIVATE_KEY_PATH=$(echo "${DOMAIN}" | cut -d':' -f2)
+        # Check if the domain already exists
+        if grep -wq "${DOMAIN_NAME}" "${CONFIG_FILE}"; then
+            # echo "Domain ${DOMAIN_NAME} already exists, skipping configuration."
+            continue
+        fi
+        # Insert domain configuration between #BT_DOMAIN_DKIM_BEGIN and #BT_DOMAIN_DKIM_END
+        sed -i "/^#BT_DOMAIN_DKIM_BEGIN$/a #${DOMAIN_NAME}_DKIM_BEGIN\n  ${DOMAIN_NAME} {\n    selectors [\n     {\n       path: \"${PRIVATE_KEY_PATH}\";\n       selector: \"default\"\n     }\n   ]\n }\n#${DOMAIN_NAME}_DKIM_END" "${CONFIG_FILE}"
+        # echo "Domain ${DOMAIN_NAME} has been successfully added to the configuration file."
+    done
+    # echo "DKIM configuration update completed."
+else
+    echo -e "DKIM key generation failed!"
+    exit 1
+fi
+chmod 755 -R "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/"
+EOF'
+    docker exec -i -e BILLIONMAIL_HOSTNAME=${BILLIONMAIL_HOSTNAME} ${RSPAMD_CONTAINER_NAME} bash /tmp/1.sh && rm -f /tmp/1.sh
+    DKIM_RECORD=$(docker exec ${RSPAMD_CONTAINER_NAME} cat "/var/lib/rspamd/dkim/${BILLIONMAIL_HOSTNAME}/default.pub")
+    # echo "DKIM RECORD: ${DKIM_RECORD}"
+}
+
+
+Domain_record() {
+
+    IPV4_ADDRESS=$(curl -sS -4 --connect-timeout 10 -m 20 https://ifconfig.me)
+    if [ -z "${IPV4_ADDRESS}" ]; then
+        IPV4_ADDRESS=$(curl -sSk --connect-timeout 10 -m 20 https://www.aapanel.com/api/common/getClientIP)
+    fi
+    ipv4_regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+    if [[ ${IPV4_ADDRESS} =~ ${ipv4_regex} ]]; then        
+        echo "${IPV4_ADDRESS}" >/dev/null 2>&1
+    elif [ -z "${IPV4_ADDRESS}" ]; then
+        IPV4_ADDRESS="YOUR_SERVER_IPV4_ADDRESS"
+    fi
+    echo -e ""
+    echo -e "\e[31mPlease add the following record to your domain name\e[0m"
+    echo -e "==========================================================="
+    echo -e " Type | Host record    |    IPv4 address   |"
+    echo -e "  \e[1;33mA\e[0m   | \e[1;33mmail.${BILLIONMAIL_HOSTNAME}\e[0m | \e[1;33m${IPV4_ADDRESS}\e[0m |"
+    echo -e "==========================================================="
+    echo -e " Type | Host record | MX priority |  Record value    "
+    echo -e "  \e[1;33mMX\e[0m  |     \e[1;33m@\e[0m       |      \e[1;33m10\e[0m     | \e[1;33mmail.${BILLIONMAIL_HOSTNAME}\e[0m "
+    echo -e "==========================================================="
+    if [ "${IPV4_ADDRESS}" ]; then
+        echo -e " Type | Host record |    Record value   |"
+        echo -e "  \e[1;33mTXT\e[0m |     \e[1;33m@\e[0m       | \e[1;33mv=spf1 +a +mx +ip4:${IPV4_ADDRESS} -all\e[0m |"
+    else
+        echo -e " Type | Host record |    Record value   |"
+        echo -e "  \e[1;33mTXT\e[0m |     \e[1;33m@\e[0m       | \e[1;33mv=spf1 +a +mx -all\e[0m |"
+    fi
+    echo -e "==========================================================="
+    echo -e " Type | Host record |    Record value     |"
+    echo -e "  \e[1;33mTXT\e[0m |   \e[1;33m_dmarc\e[0m    | \e[1;33mv=DMARC1;p=quarantine;rua=mailto:admin@${BILLIONMAIL_HOSTNAME}\e[0m |"
+    echo -e "==========================================================="
+
+    Domain_DKIM_record
+
+    if [ "${DKIM_RECORD}" ]; then
+        DKIM_RECORD=$(echo "${DKIM_RECORD}" | awk -F'"' '{print $2 $4}' | tr -d '[:space:]')
+        #echo "DKIM RECORD: ${DKIM_RECORD}"
+        echo -e " Type |  Host record       |    Record value     |"
+        echo -e "  \e[1;33mTXT\e[0m | \e[1;33mdefault._domainkey\e[0m | \e[1;33m${DKIM_RECORD}\e[0m |<-- Start from \"v=DKIM1\" end, A single line."
+        echo -e "==========================================================="
+    else
+        echo -e "${BILLIONMAIL_HOSTNAME} DKIM key generation failed!"
+    fi
+}
+
 Init_Billionmail()
 {
     # SQL file path
@@ -1035,6 +1150,7 @@ Init_Billionmail()
             VALUES ('${BILLIONMAIL_HOSTNAME}', 'mail.${BILLIONMAIL_HOSTNAME}', 500, 5368709120, 5368709120, 12, ${create_time}, 1);"
             if [ $? -eq 0 ]; then
                 echo "Domain creation was successful!"
+                Domain_DKIM_record
             else
                 Red_Error "Domain creation failed!"
             fi
@@ -1227,28 +1343,9 @@ Install_Main(){
 
 
 Install_Main
-
-IPV4_ADDRESS=$(curl -sS -4 --connect-timeout 10 -m 20 https://ifconfig.me)
-if [ -z "${IPV4_ADDRESS}" ]; then
-    IPV4_ADDRESS=$(curl -sSk --connect-timeout 10 -m 20 https://www.aapanel.com/api/common/getClientIP)
-fi
-ipv4_regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
-if [[ ${IPV4_ADDRESS} =~ ${ipv4_regex} ]]; then        
-    echo "${IPV4_ADDRESS}" >/dev/null 2>&1
-elif [ -z "${IPV4_ADDRESS}" ]; then
-    IPV4_ADDRESS="YOUR_SERVER_IPV4_ADDRESS"
-fi
-echo -e ""
-echo -e "\e[31mPlease add the following record to your domain name\e[0m"
-echo -e "==========================================================="
-echo -e " Type | Host record       |    IPv4 address      |"
-echo -e "  A   | mail.${BILLIONMAIL_HOSTNAME} | ${IPV4_ADDRESS} |"
-echo -e "==========================================================="
-echo -e " Type | Host record |    Record value    |     MX priority"
-echo -e "  MX  |     @       | mail.${BILLIONMAIL_HOSTNAME} |  10"
-echo -e "==========================================================="
+Domain_record
 
 echo -e "\e[31mPlease save the following information:\e[0m"
-echo -e "Mailbox (e-mail): \e[33m${mailbox}@${BILLIONMAIL_HOSTNAME}\e[0m \nPassword: \e[33m${Generate_mailbox_password}\e[0m"
+echo -e "Mailbox (e-mail): \e[1;33m${mailbox}@${BILLIONMAIL_HOSTNAME}\e[0m Password: \e[1;33m${Generate_mailbox_password}\e[0m"
 
-echo -e "Tip: Use \e[33mbash mail_users.sh\e[0m to add and delete Domain and Mailboxes"
+echo -e "Tip: Use \e[33mbash mail_users.sh\e[0m to Add, Delete Domain and Mailboxes etc."

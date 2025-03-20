3aa83b9eac5822bac187240536c180d6e1ccca97
Jack <fan@aapanel.com>
2025-03-20 17:56:29 +0800

    Set default database configurations.

--- core/main.go
+++ core/main.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	_ "billionmail-core/internal/packed"
+	"fmt"
+	"github.com/gogf/gf/v2/database/gdb"
 
 	"github.com/gogf/gf/v2/os/gctx"
 
 	"billionmail-core/internal/cmd"
+	"billionmail-core/internal/service/public"
 )
 
 func main() {
+	dbPass, err := public.ReadFile("./DBPASS_file.pl")
+
+	if err != nil {
+		panic(fmt.Errorf("Read database password failed: %v", err))
+	}
+
+	// Setting database configuration
+	_ = gdb.SetConfig(gdb.Config{
+		"default": gdb.ConfigGroup{
+			gdb.ConfigNode{
+				Host: "pgsql",
+				Port: "5432",
+				User: "billionmail",
+				Pass: dbPass,
+				Name: "billionmail",
+				Type: "pgsql",
+				Role: "master",
+			},
+		},
+	})
+
 	cmd.Main.Run(gctx.GetInitCtx())
 }

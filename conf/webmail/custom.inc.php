a80eb80a88a60967b314cf361764e1f3093c737e
aapanel.com <fan@aapanel.com>
2025-03-24 15:56:24 +0800

    Add Webmail configuration
    Add Webmail configuration

---
+++ conf/webmail/custom.inc.php
@@ -0,0 +1,15 @@
+<?php
+    # Product name
+    $config['product_name'] = 'Billion-Mail';
+
+    $config['mime_types'] = '/var/roundcube/config/mime.types';
+       
+    # Plugins
+    $config['plugins'] = array('password','userinfo','newmail_notifier','emoticons','zipdownload');
+
+    # Password
+    $config['password_query'] = 'update mailbox set password = %P where username = %u';
+    $config['password_algorithm'] = 'md5-crypt';
+
+
+# Please create an extra file: extra.php. Used to persist overrides Configuration

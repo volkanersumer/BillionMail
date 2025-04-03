a4111684f4a6f8da4c6665f89888b304eba454de
aapanel.com <fan@aapanel.com>
2025-04-03 11:40:18 +0800

    Add core container
    1. Add core container
    2. Add fail2ban configuration

---
+++ Dockerfiles/core/core.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [ -f "/opt/billionmail/.env" ]; then
+    if ! grep -q "ADMIN_USERNAME" /opt/billionmail/.env; then
+        ADMIN_USERNAME=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 2> /dev/null | head -c 8)
+
+        # Default administrator account password 
+        echo "ADMIN_USERNAME=${ADMIN_USERNAME}" >> /opt/billionmail/.env
+    fi
+
+    if ! grep -q "ADMIN_PASSWORD" /opt/billionmail/.env; then
+        ADMIN_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 2> /dev/null | head -c 8)
+        # Default administrator account password 
+        echo "ADMIN_PASSWORD=${ADMIN_PASSWORD}" >> /opt/billionmail/.env
+    fi
+fi
+
+# Generate DH params
+if [ ! -f "/etc/ssl/mail/dh.pem" ]; then
+    cp -d -n /etc/ssl/ssl-self-signed/* /etc/ssl/mail/
+fi
+
+# # Generate DH params
+# if [ ! -f "/etc/ssl/mail/dh.pem" ]; then
+#     openssl dhparam -out /etc/ssl/mail/dh.pem 2048
+# fi
+
+# Remove defaults-debian.conf
+if [ -f "/etc/fail2ban/jail.d/defaults-debian.conf" ]; then
+    rm -f /etc/fail2ban/jail.d/defaults-debian.conf
+fi
+
+
+## Initialize fail2ban when y, otherwise delete the rule
+# Enable fail2ban Access restrictions, specify that the IP exceeds the access limit
+if [[ ${FAIL2BAN_INIT} == "y" ]]; then
+    ## Copy fail2ban Jail
+    if [ ! -f "/etc/fail2ban/jail.d/core-accesslimit.conf" ]; then
+        cp -f /opt/billionmail/conf/core/fail2ban_init/jail.d/core-accesslimit.conf /etc/fail2ban/jail.d/core-accesslimit.conf
+    fi
+
+    if [ ! -f "/etc/fail2ban/jail.d/roundcube-accesslimit.conf" ]; then
+        cp -f /opt/billionmail/conf/core/fail2ban_init/jail.d/roundcube-accesslimit.conf /etc/fail2ban/jail.d/roundcube-accesslimit.conf
+    fi
+else
+    rm -f /etc/fail2ban/jail.d/core-accesslimit.conf
+    rm -f /etc/fail2ban/jail.d/roundcube-accesslimit.conf
+    echo -e "fail2ban: delete the rule"
+fi
+
+## Copy fail2ban Filter
+# if [ ! -f "/etc/fail2ban/filter.d/core-limit-filter.conf" ]; then
+#     cp -f /opt/billionmail/conf/core/fail2ban_init/filter.d/core-limit-filter.conf /etc/fail2ban/filter.d/core-limit-filter.conf
+# fi
+cp -rf /opt/billionmail/conf/core/fail2ban_init/filter.d/*.conf /etc/fail2ban/filter.d/
+
+if [ ! -d "/opt/billionmail/core/public/" ]; then
+    mkdir -p /opt/billionmail/core/public/dist
+    mkdir -p /opt/billionmail/core/public/html
+fi
+
+if [ ! -d "/opt/billionmail/core/logs/" ]; then
+    mkdir -p /opt/billionmail/core/logs
+    touch /opt/billionmail/core/logs/access-test.log
+fi
+
+chmod +x /opt/billionmail/core/billionmail
+
+exec "$@"

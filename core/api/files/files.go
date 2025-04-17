825eec2809379bd23516255c8f7f9d7482b459da
lotk <977935501@qq.com>
2025-04-17 14:33:10 +0800

    Add methods for downloading files

---
+++ core/api/files/files.go
@@ -0,0 +1,15 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package files
+
+import (
+	"context"
+
+	"billionmail-core/api/files/v1"
+)
+
+type IFilesV1 interface {
+	DownloadFile(ctx context.Context, req *v1.DownloadFileReq) (res *v1.DownloadFileRes, err error)
+}

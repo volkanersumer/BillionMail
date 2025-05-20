d5fa3e885abf5186949e1a7a6719ec0a5ecbbd23
lotk <977935501@qq.com>
2025-05-20 18:01:26 +0800

    Change an absolute path to a relative path Increase the mx value of the user's mailbox

--- core/internal/controller/mail_boxes/mail_boxes_v1_get_all_mailbox.go
+++ core/internal/controller/mail_boxes/mail_boxes_v1_get_all_mailbox.go
@@ -1,11 +1,10 @@
 package mail_boxes
 
 import (
+	"billionmail-core/api/mail_boxes/v1"
 	"billionmail-core/internal/service/mail_boxes"
 	"context"
 	"fmt"
-
-	"billionmail-core/api/mail_boxes/v1"
 )
 
 func (c *ControllerV1) GetAllMailbox(ctx context.Context, req *v1.GetAllMailboxReq) (res *v1.GetAllMailboxRes, err error) {

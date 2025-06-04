f82da21e7e6ffc759fefecc1298c72735b426fa4
zan <125946751+zan-12@users.noreply.github.com>
2025-06-04 12:39:04 +0900

    Update index.ts

--- core/frontend/src/i18n/index.ts
+++ core/frontend/src/i18n/index.ts
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import en from './lang/en.json'
 import zh from './lang/zh.json'
+import ja from './lang/ja.json'
 
 const i18n = createI18n({
 	legacy: false,
@@ -10,12 +11,13 @@ const i18n = createI18n({
 	messages: {
 		en,
 		zh,
+		ja,
 	},
 })
 
 // 提供切换语言的方法
 export const setLanguage = (locale: string) => {
-	i18n.global.locale.value = locale as 'zh' | 'en'
+	i18n.global.locale.value = locale as 'en' | 'zh' | 'ja'
 }
 
 export default i18n

a3f7d2c1583d453aa75200041c548255bf0b23ef
lotk <977935501@qq.com>
2025-06-10 15:13:29 +0800

    Automatically request a certificate when creating a sender domain

--- core/internal/controller/domains/domains_v1_add_domain.go
+++ core/internal/controller/domains/domains_v1_add_domain.go
@@ -2,7 +2,11 @@ package domains
 
 import (
 	"billionmail-core/internal/service/domains"
+	"billionmail-core/internal/service/rbac"
 	"context"
+	"github.com/gogf/gf/os/gtimer"
+	"github.com/gogf/gf/v2/frame/g"
+	"time"
 
 	"billionmail-core/api/domains/v1"
 )
@@ -22,6 +26,20 @@ func (c *ControllerV1) AddDomain(ctx context.Context, req *v1.AddDomainReq) (res
 	if err = domains.Add(ctx, domain); err != nil {
 		return nil, err
 	}
+
+	// Automatic Certificate Application
+	gtimer.AddOnce(500*time.Millisecond, func() {
+		acc, accErr := rbac.GetCurrentAccount(ctx)
+		if accErr == nil {
+			certErr := domains.ApplyLetsEncryptCertWithHttp(ctx, req.Domain, acc)
+			if certErr != nil {
+				g.Log().Warningf(ctx, "Domain name [%s] added successfully, but auto-request certificate failed: %v", req.Domain, certErr)
+			}
+		} else {
+			g.Log().Warningf(ctx, "The domain name [%s] was added successfully, but the current account failed to obtain the certificate automatically: %v", req.Domain, accErr)
+		}
+
+	})
 
 	res.SetSuccess("Domain added successfully")
 	return

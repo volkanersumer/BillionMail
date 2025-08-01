e4efc6b3c4b9a4c4f87aa749df75962659e2ec2e
zdm <1946442364@qq.com>
2025-08-01 16:07:37 +0800

    New error logs added to the log page

--- core/frontend/src/views/logs/error/index.vue
+++ core/frontend/src/views/logs/error/index.vue
@@ -1,11 +1,13 @@
 <template>
-	<div>
-		<div class="mb-16px">
+	<div class="error-logs-container">
+		<div class="logs-header">
 			<n-button type="primary" @click="getLogs">{{ $t('common.actions.refresh') }}</n-button>
 		</div>
-		<n-spin :show="loading">
-			<bt-logs :code="content" height="640px" :scroll-to-bottom="true"></bt-logs>
-		</n-spin>
+		<div class="logs-content min-h-300px">
+			<n-spin :show="loading">
+				<bt-logs :code="content" height="100%" :scroll-to-bottom="true"></bt-logs>
+			</n-spin>
+		</div>
 	</div>
 </template>
 
@@ -14,7 +16,6 @@ import { isString } from '@/utils'
 import { getErrorLogs } from './controller'
 
 const content = ref('')
-
 const loading = ref(false)
 
 const getLogs = async () => {
@@ -30,4 +31,34 @@ const getLogs = async () => {
 getLogs()
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.error-logs-container {
+	display: flex;
+	flex-direction: column;
+	height: calc(100vh - 200px); // 减去布局的 paddingTop 和页面 padding
+	box-sizing: border-box;
+}
+
+.logs-header {
+	flex-shrink: 0;
+	margin-bottom: 16px;
+}
+
+.logs-content {
+	flex: 1;
+	min-height: 0; // 防止 flex 子元素溢出
+	display: flex;
+	flex-direction: column;
+
+	:deep(.n-spin-container) {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	:deep(.code-container) {
+		flex: 1;
+		min-height: 0;
+	}
+}
+</style>
